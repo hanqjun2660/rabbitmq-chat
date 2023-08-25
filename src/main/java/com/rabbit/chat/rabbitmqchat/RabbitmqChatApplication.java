030a5decce59189a9307f6ebda8f62deabe754f2
KANG\b2000 <hanqjun2660@gmail.com>
2023-08-25 14:45:31 +0900

    최초커밋

---
+++ src/main/java/com/rabbit/chat/rabbitmqchat/RabbitmqChatApplication.java
@@ -0,0 +1,13 @@
+package com.rabbit.chat.rabbitmqchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rabbit/chat/rabbitmqchat/RabbitmqChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabbit.chat.rabbitmqchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
